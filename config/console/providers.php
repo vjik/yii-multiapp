40e1b3b1d8a0967d3d583063a7cc58d4637ff003
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-07-30 13:10:19 +0300

    Added yii-console.

---
+++ config/console/providers.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/* @var array $params */
+
+use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
+
+return [
+    ReverseBlockMerge::class => new ReverseBlockMerge(),
+];
