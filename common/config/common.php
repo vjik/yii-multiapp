c4553f8af4e9b857f167d6a7123984d0a70959c0
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-09-05 14:32:32 +0300

    Remove CycleFactory.

--- common/config/common.php
+++ common/config/common.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 /* @var array $params */
 
-use Cycle\ORM\Factory;
-use Cycle\ORM\FactoryInterface;
 use Cycle\ORM\ORMInterface;
 use Domain\Company\Entity\Employee;
 use Domain\Company\Repository\EmployeeRepository;
 use Psr\Container\ContainerInterface;
-use Spiral\Database\DatabaseManager;
 use Yiisoft\Aliases\Aliases;
 
 return [
@@ -22,16 +19,6 @@ return [
         '__class' => Aliases::class,
         '__construct()' => [$params['aliases']],
     ],
-
-    /** @todo Remove after fix https://github.com/yiisoft/yii-cycle/issues/50 */
-    FactoryInterface::class => static function (ContainerInterface $container) {
-        return new Factory(
-            $container->get(DatabaseManager::class),
-            null,
-            $container->get(\Common\CycleFactory::class),
-            $container
-        );
-    },
 
     EmployeeRepository::class => static function (ContainerInterface $container) {
         return $container->get(ORMInterface::class)->getRepository(Employee::class);
